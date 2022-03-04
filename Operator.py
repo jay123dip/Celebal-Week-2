74bec774f5e75739650216b691377af13fa889df
jay123dip <89289785+jay123dip@users.noreply.github.com>
2022-03-05 00:05:05 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,17 @@
+x=int(input())
+#Arithmetic operator
+print(x+5)#addition
+print(x-5)#Substraction
+print(x*5)#Multiplication
+print(x/5)#Division
+print(x%5)#Modulus
+#Bitwise Operator
+print(x&5)#And
+print(x|5)#Or
+print(~x)#Not
+print(x^5)#Xor
+print(x>>5)#Right shift
+print(x<<5)#left shift
+#boolian
+print(x>10)
+print(x>3)
